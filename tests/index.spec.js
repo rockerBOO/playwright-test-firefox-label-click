29babc47bc1c04de968fa0919c6beea5071569be
rockerBOO <rockerboo@gmail.com>
2024-10-23 22:27:13 -0400

    Add github action

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,16 +1,17 @@
 import { test, expect } from "@playwright/test";
 
-test("click a label", async ({ page }) => {
-  page.on("pageerror", (error) => {
-    console.error("Page error:", error.message);
-  });
-  await page.goto("/");
-  await page.getByText("Choose a file").click();
-});
+// test("click a label", async ({ page }) => {
+//   page.on("pageerror", (error) => {
+//     console.error("Page error:", error.message);
+//   });
+//   await page.goto("/");
+//   await page.getByText("Choose a file", {exact: true}).click();
+// });
 
 test("index", async ({ page }) => {
   page.on("pageerror", (error) => {
     console.error("Page error:", error.message);
   });
   await page.goto("/");
+  await page.getByText("A second file").click();
 });
