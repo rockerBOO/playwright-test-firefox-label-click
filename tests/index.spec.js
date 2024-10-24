8365409f48417ceb87a14ac978244ab00fa150aa
rockerBOO <rockerboo@gmail.com>
2024-10-23 21:35:14 -0400

    Add simple working test for firefox

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,9 +1,16 @@
 import { test, expect } from "@playwright/test";
 
-test("test", async ({ page }) => {
+test("click a label", async ({ page }) => {
   page.on("pageerror", (error) => {
     console.error("Page error:", error.message);
   });
   await page.goto("/");
   await page.getByText("Choose a file").click();
+});
+
+test("index", async ({ page }) => {
+  page.on("pageerror", (error) => {
+    console.error("Page error:", error.message);
+  });
+  await page.goto("/");
 });
